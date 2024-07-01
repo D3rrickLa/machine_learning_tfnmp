a4ac75d992f1b3de85f9dc97f3a9cca1ea4ebb1f
D3rrick <derrickla76@gmail.com>
2024-07-01 17:25:31 -0400

    finished programming the camera_capture, still slow compared to the development model (m6)

--- model/LSTM/camera_capture.py
+++ model/LSTM/camera_capture.py
@@ -13,24 +13,22 @@ class Landmarks():
         self.HandLandmarker = vision.HandLandmarker 
         self.HandLandmarkerOptions = vision.HandLandmarkerOptions 
         self.HandLandmarkerResult = None
-        self.VisionRunningMode = vision.RunningMode
         self.createLandmarker()
-        self.frame = None
 
     def createLandmarker(self):
         def update_result(result: vision.HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
             self.HandLandmarkerResult = result
-            # print('hand landmarker result: {}'.format(result))
             
         self.BaseOptions = python.BaseOptions
         
         landmark_options = self.HandLandmarkerOptions(
             base_options = self.BaseOptions(model_asset_path="model/hand_landmarker.task"),
-            running_mode = self.VisionRunningMode.LIVE_STREAM,
+            running_mode = vision.RunningMode.LIVE_STREAM,
             num_hands = 2, 
-            min_hand_presence_confidence=0.80, 
-            min_tracking_confidence=0.80,
-            result_callback=update_result
+            min_hand_detection_confidence = 0.60,
+            min_hand_presence_confidence= 0.60, 
+            min_tracking_confidence = 0.70,
+            result_callback = update_result
         )
 
         self.HandLandmarker = self.HandLandmarker.create_from_options(landmark_options)
@@ -39,17 +37,13 @@ class Landmarks():
         try:
             mp_image = mp.Image(image_format = mp.ImageFormat.SRGB, data = frame)
             self.HandLandmarker.detect_async(mp_image, timestamp_ms = int(time.time() * 1000))
-
             return self.HandLandmarkerResult
+
         except Exception as e:
-            raise e
+            print("Error in detect_landmarks:", e)
+            return None
 
 def annotate_image(image, landmarker_result):
-    MARGIN = 10  # pixels
-    FONT_SIZE = 1
-    FONT_THICKNESS = 1
-    HANDEDNESS_TEXT_COLOR = (212, 130, 173)
-
     if landmarker_result is None or not landmarker_result.hand_landmarks:
         return image
 
@@ -64,11 +58,12 @@ def annotate_image(image, landmarker_result):
         hand_landmarks_proto.landmark.extend([
         landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks])
         mp.solutions.drawing_utils.draw_landmarks(
-        annotated_image,
-        hand_landmarks_proto,
-        mp.solutions.hands.HAND_CONNECTIONS,
-        mp.solutions.drawing_styles.get_default_hand_landmarks_style(),
-        mp.solutions.drawing_styles.get_default_hand_connections_style())
+            annotated_image,
+            hand_landmarks_proto,
+            mp.solutions.hands.HAND_CONNECTIONS,
+            mp.solutions.drawing_styles.get_default_hand_landmarks_style(),
+            mp.solutions.drawing_styles.get_default_hand_connections_style()
+        )
         
     return annotated_image
 
@@ -81,9 +76,9 @@ def main():
         if not ret:
             break
         
-        X = landmarker.detect_async(frame)
-        if X and X.hand_landmarks:
-            anno_frame = annotate_image(frame, X)
+        landmarker_result = landmarker.detect_async(frame)
+        if landmarker_result:
+            anno_frame = annotate_image(frame, landmarker_result)
         else:
             anno_frame = frame
 
