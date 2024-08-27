db12e5d8296ef9650d00d01afc748432229b9e95
D3rrick <derrickla76@gmail.com>
2024-08-27 18:49:57 -0400

    working on website demo

--- website/backend/controllers/rest_controller.py
+++ website/backend/controllers/rest_controller.py
@@ -1,6 +1,10 @@
+import os
+import cv2
 from fastapi import FastAPI, Request, WebSocket 
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates 
-from fastapi.responses import StreamingResponse, PlainTextResponse
+from fastapi.responses import FileResponse, StreamingResponse, PlainTextResponse
+import numpy as np
 import uvicorn 
 import requests 
 import asyncio
@@ -8,16 +12,34 @@ import asyncio
 import uvicorn.config 
 
 app = FastAPI()
-
-
+app.mount("/static", StaticFiles(directory=r"./website/backend/static"), name="static")
+save_dir = r"./website/backend/saved_images"
 @app.get("/")
 def index():
-    return {"this is working": True}
+    return {"message" : "server is working properly"}
 
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    return FileResponse("static/favicon.ico")
 
+@app.post("/process_video")
+async def process_video(request: Request):
+    data = await request.body()
+    np_arr = np.frombuffer(data, np.uint8)
+    frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+    if frame is None:
+        return {"error": "Failed to decode image frame."}
+
+    # Define the file path where the frame will be saved
+    frame_filename = os.path.join(save_dir, "frame.jpg")
+
+    # Save the frame as a .jpg image
+    cv2.imwrite(frame_filename, frame)
+    return {"message" : "Video processed successfully"}
 
 def main():
-    config = uvicorn.Config("rest_controller:app", host="localhost", port=8000, reload=True)
+    config = uvicorn.Config("rest_controller:app", host="localhost", port=8001, reload=True)
     sever  = uvicorn.Server(config)
     sever.run()
 
--- website/frontend/controllers/main.py
+++ website/frontend/controllers/main.py
@@ -1,9 +1,10 @@
 import cv2
+from fastapi.staticfiles import StaticFiles
 import numpy as np
 import uvicorn
 from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect 
 from fastapi.templating import Jinja2Templates 
-from fastapi.responses import HTMLResponse, StreamingResponse, PlainTextResponse 
+from fastapi.responses import FileResponse, HTMLResponse, StreamingResponse, PlainTextResponse 
 import requests 
 import asyncio 
 import threading 
@@ -13,7 +14,9 @@ import threading
 
 
 app = FastAPI(debug=True)
+app.mount("/static", StaticFiles(directory=r"./website/frontend/static"), name="static")
 templates = Jinja2Templates(directory=r"./website/frontend/templates")
+
 
 camera = cv2.VideoCapture(0)
 camera_active = threading.Event()
@@ -22,6 +25,11 @@ camera_active.set()
 @app.get("/", response_class=HTMLResponse)
 def index(request: Request):
     return templates.TemplateResponse(name="index.html", request=request)
+
+# Route to serve the favicon
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    return FileResponse("static/favicon.ico")
 
 @app.get("/test")
 def getTable(request: Request):
@@ -33,16 +41,16 @@ async def websocket_point(websocket: WebSocket):
     try:
         while True: 
             data = await websocket.receive_bytes()
+            print(len(data))
+            response = requests.post("http://localhost:8001/process_video", data=data)
 
-            # convert byte stream to an image 
-            np_arr = np.frombuffer(data, np.uint8)
-            frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-
-
-            # do something with the code, like result = process_frame(frame)
+           
+            await websocket.send_text(response.text) # Awaiting WebSocket to send the response
 
     except WebSocketDisconnect:
         print("Client disconnected")
+    finally:
+        await websocket.close()
 
 
 def main():
@@ -52,3 +60,8 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+# TODO
+# figure out the ending of data frame frontend to backend - there's a reason why we used ffmpeg... 
